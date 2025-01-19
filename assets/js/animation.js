648816dcde3309ffcc1d0727ff90f493ecdbd5a5
MC13veterangamerprogrammingking <q20201919@outlook.com>
2025-01-19 10:19:31 +0800

    Update animation.js

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,19 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // 获取所有需要淡入淡出的文章
-  const articles = document.querySelectorAll('.fade-in');
-
-  // 定义一个函数来使文章可见
-  function showArticles() {
-    articles.forEach(article => {
-      article.classList.add('visible');
-    });
-  }
-
-  // 在页面加载后延迟一段时间显示文章
-  setTimeout(showArticles, 500);
-});
-
-document.addEventListener("DOMContentLoaded", function() {
   // 获取所有需要动画的文章
   const articles = document.querySelectorAll('.animate');
 
